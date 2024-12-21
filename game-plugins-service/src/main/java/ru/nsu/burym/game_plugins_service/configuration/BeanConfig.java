2b7735bff42c184a4505a37cfe07790722439fac
Maks <m.burym@g.nsu.ru>
2024-12-22 01:05:08 +0700

    add logging

--- game-plugins-service/src/main/java/ru/nsu/burym/game_plugins_service/configuration/BeanConfig.java
+++ game-plugins-service/src/main/java/ru/nsu/burym/game_plugins_service/configuration/BeanConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.net.URI;
 import java.util.Objects;
@@ -45,5 +46,15 @@ public class BeanConfig {
         return new AuthConfig()
                 .withUsername(environment.getProperty("dockerUsername"))
                 .withUsername(environment.getProperty("dockerPassword"));
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 }
